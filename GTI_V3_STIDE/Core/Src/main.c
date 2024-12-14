bb796980869f0c436a046b29991f6fe88d25e3aa
wilmar-zeta <111376314+wilmar-zeta@users.noreply.github.com>
2024-12-14 17:52:01 +0800

    Update main.c

--- GTI_V3_STIDE/Core/Src/main.c
+++ GTI_V3_STIDE/Core/Src/main.c
@@ -579,6 +579,20 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 			Cos_50_PID.setpoint = 70.0f;
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* USER CODE END 4 */
 
 /**
@@ -603,9 +617,6 @@ void Error_Handler(void)
   */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+  
 }
 #endif /* USE_FULL_ASSERT */
